35974c60925d7a7c786aa465f0c538d7ab582358
David Matthew <davidmatthew.ie@gmail.com>
2023-03-12 21:31:12 +0000

    Updated template sketch

--- sketches/00-template/sketch.js
+++ sketches/00-template/sketch.js
@@ -1,4 +1,4 @@
-import { SvJs } from '../../node_modules/svjs/src/index.js';
+import { SvJs, Gen } from '../../../svjs/src/index.js';
 
 // Viewport size (1:1 aspect ratio).
 const svgSize = window.innerWidth > window.innerHeight ? window.innerHeight : window.innerWidth;
@@ -11,4 +11,3 @@ svg.set({ width: svgSize, height: svgSize, viewBox: '0 0 1000 1000' });
 svg.create('rect').set({
 	x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
 });
-

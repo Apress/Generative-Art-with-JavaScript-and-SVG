c211a3d195c0c830b030a6031b2fb5e85f1212e2
David Matthew <davidmatthew.ie@gmail.com>
2023-03-18 19:53:41 +0000

    Updated sketch

--- sketches/08-elements-everywhere/sketch.js
+++ sketches/08-elements-everywhere/sketch.js
@@ -12,18 +12,29 @@ svg.create('rect').set({
 	x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
 });
 
+// Set a random iteration count for the loop.
 let iterations = Gen.random(250, 500, false);
+
+// Our shapes array.
 let elements = ['circle', 'line', 'rect'];
 
+// Start the loop.
 for (let i = 0; i < iterations; i += 1) {
+
+	// Pick a random element.
 	let element = Gen.random(elements);
+
+	// Set up variables that we can use an any element.
 	let x = Gen.random(200, 800);
 	let y = Gen.random(200, 800);
 	let fill = `hsl(${Gen.random(120, 240)} 80% 80% / ${Gen.random(5, 40)}%)`;
 	let stroke = `hsl(${Gen.random(0, 120)} 80% 80% / ${Gen.random(5, 40)}%)`;
 	let strokeWidth = `${Gen.random(1, 3)}`;
-	let props;
 
+	// Initialise the properties variable.
+	let props;
+	
+	// Populate the properties depending on the element chosen.
 	switch(element) {
 		case 'circle':
 			props = {
@@ -57,5 +68,6 @@ for (let i = 0; i < iterations; i += 1) {
 			}
 	}
 
+	// Create the element and set its properties.
   svg.create(element).set(props);
 }

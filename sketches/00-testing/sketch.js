2286ed1fe8e421417e67304815821dd5dda0c4c2
David Matthew <davidmatthew.ie@gmail.com>
2023-06-20 15:24:54 +0100

    New example sketch

--- sketches/00-testing/sketch.js
+++ sketches/00-testing/sketch.js
@@ -1,4 +1,4 @@
-import { SvJs, Gen, Noise } from '../../../svjs/src/index.js';
+import { SvJs, Gen } from '../../../svjs/src/index.js';
 
 // Viewport size (1:1 aspect ratio).
 const svgSize = Math.min(window.innerWidth, window.innerHeight);
@@ -12,65 +12,72 @@ svg.create('rect').set({
   x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
 });
 
-for (let i = 0; i < 60; i += 1) {
-  let star1 = svg.create('path');
-  star1.set({
-    fill: 'none',
-    stroke: `hsl(${60 + (i * 2)} 80% 80% / 0.5)`,
-    stroke_width: 0.5,
-    d: star(500, 500, 10 + (i * 5), 25 + (i * 5), 12)
-  });
-
-  star1.rotate(i * 1.5);
-
-  let star2 = svg.create('path');
-  star2.set({
-    fill: 'none',
-    stroke: `hsl(${360 - (i * 2)} 80% 80% / 0.5)`,
-    stroke_width: 0.5,
-    d: star(500, 500, 10 + (i * 5), 25 + (i * 5), 12)
-  });
-
-  star2.rotate(i * - 1.5);
-}
-
-
-function star(cx, cy, r1, r2, nPoints) {
-  let pathData = '';
-  let angle = (Math.PI * 2) / nPoints;
-  
-  for (let i = 0; i < Math.PI * 2; i += angle) {
-    let sx = cx + Math.cos(i) * r2;
-    let sy = cy + Math.sin(i) * r2;
-    pathData = (i === 0) ? `M ${sx} ${sy} ` : `${pathData} L ${sx} ${sy} `;
-    
-    sx = cx + Math.cos(i + angle / 2) * r1;
-    sy = cy + Math.sin(i + angle / 2) * r1;
-    pathData += `L ${sx} ${sy} `;
+svg.create('style').content(`
+  path {
+    fill: none;
+    stroke-width: 5
   }
+`);
 
-  pathData += ' Z';
+svg.create('path').set({
+  stroke: 'red',
+  d: 'M 350 500 A 160 80 0 0 0 550 500'
+});
 
-  return pathData;
+svg.create('path').set({
+  stroke: 'yellow',
+  d: 'M 350 500 A 160 80 0 1 0 550 500'
+});
 
-}
+svg.create('path').set({
+  stroke: 'green',
+  d: 'M 350 500 A 160 80 0 0 1 550 500'
+});
 
-function bezierStar(cx, cy, r1, r2, nPoints) {
-  let pathData = '';
-  let angle = (Math.PI * 2) / nPoints;
+svg.create('path').set({
+  stroke: 'purple',
+  d: 'M 350 500 A 160 80 0 1 1 550 500'
+});
+
+
+
+
+// function star(cx, cy, r1, r2, nPoints) {
+//   let pathData = '';
+//   let angle = (Math.PI * 2) / nPoints;
   
-  for (let i = 0; i < Math.PI * 2; i += angle) {
-    let sx = cx + Math.cos(i) * r2;
-    let sy = cy + Math.sin(i) * r2;
-    pathData = (i === 0) ? `M ${sx} ${sy} ` : `${pathData} L ${sx} ${sy} `;
+//   for (let i = 0; i < Math.PI * 2; i += angle) {
+//     let sx = cx + Math.cos(i) * r2;
+//     let sy = cy + Math.sin(i) * r2;
+//     pathData = (i === 0) ? `M ${sx} ${sy} ` : `${pathData} L ${sx} ${sy} `;
     
-    sx = cx + Math.cos(i + angle / 2) * r1;
-    sy = cy + Math.sin(i + angle / 2) * r1;
-    pathData += `L ${sx} ${sy} `;
-  }
+//     sx = cx + Math.cos(i + angle / 2) * r1;
+//     sy = cy + Math.sin(i + angle / 2) * r1;
+//     pathData += `L ${sx} ${sy} `;
+//   }
 
-  pathData += ' Z';
+//   pathData += ' Z';
 
-  return pathData;
+//   return pathData;
 
-}
+// }
+
+// function bezierStar(cx, cy, r1, r2, nPoints) {
+//   let pathData = '';
+//   let angle = (Math.PI * 2) / nPoints;
+  
+//   for (let i = 0; i < Math.PI * 2; i += angle) {
+//     let sx = cx + Math.cos(i) * r2;
+//     let sy = cy + Math.sin(i) * r2;
+//     pathData = (i === 0) ? `M ${sx} ${sy} ` : `${pathData} L ${sx} ${sy} `;
+    
+//     sx = cx + Math.cos(i + angle / 2) * r1;
+//     sy = cy + Math.sin(i + angle / 2) * r1;
+//     pathData += `L ${sx} ${sy} `;
+//   }
+
+//   pathData += ' Z';
+
+//   return pathData;
+
+// }
---
+++ sketches/15-generative-arcs/sketch.js
@@ -0,0 +1,39 @@
+import { SvJs, Gen } from '../../../svjs/src/index.js';
+
+// Viewport size (1:1 aspect ratio).
+const svgSize = Math.min(window.innerWidth, window.innerHeight);
+
+// Parent SVG.
+const svg = new SvJs().addTo(document.getElementById('container'));
+svg.set({ width: svgSize, height: svgSize, viewBox: '0 0 1000 1000' });
+
+// Background.
+svg.create('rect').set({
+  x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
+});
+
+let arcs = svg.create('g');
+let rx = Gen.random(-25, 25).toFixed(2);
+let ry = Gen.random(-50, 50).toFixed(2);
+let hue = Gen.random(0, 360, false);
+
+for (let i = 0; i < 360; i += 1) {
+  let rotation = Gen.random(0, 180, false);
+  let large = Gen.chance() ? 1 : 0;
+
+  arcs.create('path').set({
+    fill: 'none',
+    stroke: `hsl(${hue} 75% 75% / 0.05)`,
+    d: `M 275 500 A ${rx} ${ry} ${rotation} ${large} 0 725 500`
+  });
+
+  arcs.create('path').set({
+    fill: 'none',
+    stroke: `hsl(${hue + 60} 75% 75% / 0.05)`,
+    d: `M 275 500 A ${rx} ${ry} ${rotation} ${large} 1 725 500`
+  });
+
+  hue = (hue % 360) + 0.5;
+}
+
+arcs.rotate(Gen.random(0, 360, false));

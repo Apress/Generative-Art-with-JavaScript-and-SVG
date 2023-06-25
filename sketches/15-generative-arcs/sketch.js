dbdf192c95b2be3d93218c885646d8110a98efe3
David Matthew <davidmatthew.ie@gmail.com>
2023-06-25 21:22:58 +0100

    Corrected error in comments

--- sketches/15-generative-arcs/sketch.js
+++ sketches/15-generative-arcs/sketch.js
@@ -16,8 +16,8 @@ svg.create('rect').set({
 let arcs = svg.create('g');
 
 // Randomise some variables.
-let rx = Gen.random(-25, 25).toFixed(2);
-let ry = Gen.random(-50, 50).toFixed(2);
+let rx = Gen.random(5, 350).toFixed(2);
+let ry = Gen.random(5, 350).toFixed(2);
 let hue = Gen.random(0, 360, false);
 
 // Create two sets of elliptical arc curves on each iteration.
@@ -25,20 +25,20 @@ for (let i = 0; i < 360; i += 1) {
 
   // Randomise the rotation and large arc flag.
   let rotation = Gen.random(0, 180, false);
-  let large = Gen.chance() ? 1 : 0;
+  let largeArc = Gen.chance() ? 1 : 0;
 
-  // Create a first set of clockwise arc curves (sweep = 0).
+  // Create a first set of clockwise arc curves (sweep = 1).
   arcs.create('path').set({
     fill: 'none',
     stroke: `hsl(${hue} 75% 75% / 0.05)`,
-    d: `M 275 500 A ${rx} ${ry} ${rotation} ${large} 0 725 500`
+    d: `M 275 500 A ${rx} ${ry} ${rotation} ${largeArc} 1 725 500`
   });
 
-  // Create a second set of counter-clockwise arc curves (sweep = 1).
+  // Create a second set of counter-clockwise arc curves (sweep = 0).
   arcs.create('path').set({
     fill: 'none',
     stroke: `hsl(${hue + 60} 75% 75% / 0.05)`,
-    d: `M 275 500 A ${rx} ${ry} ${rotation} ${large} 1 725 500`
+    d: `M 275 500 A ${rx} ${ry} ${rotation} ${largeArc} 0 725 500`
   });
 
   // Increment the hue.

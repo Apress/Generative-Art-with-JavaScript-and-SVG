89d8083c72fd67892897fc8b9c18277aba322378
David Matthew <davidmatthew.ie@gmail.com>
2023-11-24 23:08:17 +0000

    Updated template sketch

--- sketches/00-template/sketch.js
+++ sketches/00-template/sketch.js
@@ -1,4 +1,4 @@
-import { SvJs, Gen } from '../../../svjs/src/index.js';
+import { SvJs, Gen } from '../../node_modules/svjs/src/index.js';
 
 // Viewport size (1:1 aspect ratio).
 const svgSize = Math.min(window.innerWidth, window.innerHeight);

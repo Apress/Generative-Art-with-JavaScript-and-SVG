eab9f565cacba8739774e3e3a1e924192142b8d8
David Matthew <davidmatthew.ie@gmail.com>
2023-12-10 22:53:08 +0000

    Refactored and updated chapter 7 examples

--- sketches/16-organic-curves/sketch.js
+++ sketches/16-organic-curves/sketch.js
@@ -1,15 +1,15 @@
 import { SvJs, Gen, Noise } from '../../node_modules/svjs/src/index.js';
 
-// Viewport size (1:1 aspect ratio).
-const svgSize = Math.min(window.innerWidth, window.innerHeight);
-
 // Parent SVG.
 const svg = new SvJs().addTo(document.getElementById('container'));
+
+// Viewport and viewBox (1:1 aspect ratio).
+const svgSize = Math.min(window.innerWidth, window.innerHeight);
 svg.set({ width: svgSize, height: svgSize, viewBox: '0 0 1000 1000' });
 
 // Background.
 svg.create('rect').set({
-	x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
+  x: 0, y: 0, width: 1000, height: 1000, fill: '#181818'
 });
 
 // Noise-related.
